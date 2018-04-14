8edd2c88690599474f45f0a3121eee0ea3510b10
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2018-04-14 11:14:02 +0200

    shop activity - layout resources

--- app/src/main/java/com/szymon/hackathonapplication/activities/ShopActivity.java
+++ app/src/main/java/com/szymon/hackathonapplication/activities/ShopActivity.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.szymon.hackathonapplication.R;
+
 public class ShopActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+//        ButterKnife.bind(this);
     }
 
 }
