ec40487d1d07859182c73d87762c82e9d3e698cb
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2018-04-14 10:35:37 +0200

    shop activity - definition in manifest.xml

---
+++ app/src/main/java/com/szymon/hackathonapplication/activities/ShopActivity.java
@@ -0,0 +1,14 @@
+package com.szymon.hackathonapplication.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ShopActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
