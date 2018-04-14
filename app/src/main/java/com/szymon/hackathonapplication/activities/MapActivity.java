deae69d75467b4891db0d42e4eb15f561a352f9b
Szymon <szymon.duraj.sp@lhsystems.com>
2018-04-14 17:14:36 +0200

    challenges working in map activity

--- app/src/main/java/com/szymon/hackathonapplication/activities/MapActivity.java
+++ app/src/main/java/com/szymon/hackathonapplication/activities/MapActivity.java
@@ -11,9 +11,11 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,6 +28,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.szymon.hackathonapplication.R;
 import com.szymon.hackathonapplication.interfaces.MapMVP;
 import com.szymon.hackathonapplication.models.challenges.Challenge;
+import com.szymon.hackathonapplication.models.challenges.PearTimeChallenge;
 import com.szymon.hackathonapplication.models.fruits.Fruit;
 import com.szymon.hackathonapplication.presenters.MapActivityPresenter;
 
@@ -34,7 +37,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import mbanje.kurt.fabbutton.FabButton;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static com.szymon.hackathonapplication.helpers.SystemServiceManager.requestFineLocationPermission;
 
 public class MapActivity extends FragmentActivity implements LocationListener, OnMapReadyCallback, MapMVP.View {
@@ -53,7 +59,15 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
     TextView challengeTitle;
     @BindView(R.id.text_challenge_current_score)
     TextView challengeCurrentProgressTextView;
+    @BindView(R.id.btn_challenges)
+    FabButton challengesButton;
+    private int challengeCount = 0;
+    private boolean challengeMode;
 
+    @OnClick(R.id.TMP_increment_pears)
+    public void tmpincrementPears() {
+        updateCurrentChallenge(currentChallenge);
+    }
 
     @OnClick(R.id.btn_challenges)
     public void goToChallengeActivity(){
@@ -89,6 +103,7 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
         presenter.loadFruits();
 
         createLocationUpdates();
+        startChallengeMode(new PearTimeChallenge());
     }
 
     private void createLocationUpdates() {
@@ -118,6 +133,18 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
         }
     }
 
+    @Override
+    public void updateCurrentChallenge(final Challenge currentChallenge) {
+        if (challengeMode) {
+            challengeCount++;
+            if (challengeCount == currentChallenge.howManyToCollect) {
+                endChallengeMode(true);
+            } else {
+                challengeCurrentProgressTextView.setText(challengeCount + "/" + currentChallenge.howManyToCollect);
+            }
+        }
+    }
+
     @OnClick(R.id.button_shop)
     public void goToShopButton() {
         startActivity(new Intent(MapActivity.this, ShopActivity.class));
@@ -129,6 +156,65 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
                 .position(fruit.location));
 
         fruitMarker.setIcon(Fruit.getMarkerIconFromDrawable(getDrawable(fruit.iconResId)));
+    }
+
+    public void startTimer(final int minutes) {
+        final int millis = minutes * 60 * 1000;
+        challengeTimerCountDown = new CountDownTimer(millis, 1000) {
+
+            public void onTick(final long millisUntilFinished) {
+                int secs = (int) (millisUntilFinished / 1000);
+                int mins = secs / 60;
+                secs = secs % 60;
+                updateTimer(getString(R.string.flight_time_format, mins, String.format("%02d", secs)));
+            }
+
+            public void onFinish() {
+                endChallengeMode(false);
+            }
+        };
+        challengeTimerCountDown.start();
+    }
+
+    private void updateTimer(final String timeLeft) {
+        challengeTimer.setText(timeLeft);
+    }
+
+    public void startChallengeMode(final Challenge challenge) {
+        challengeMode = true;
+        currentChallenge = challenge;
+        challengesButton.setClickable(false);
+        challengesButton.setEnabled(false);
+        challengesButton.setAlpha(0.5f);
+        setChallengeLayoutStyle(challenge);
+        startTimer(challenge.timeInMinutes);
+    }
+
+    private void setChallengeLayoutStyle(final Challenge challenge) {
+        challengeLayout.setVisibility(VISIBLE);
+        challengeIcon.setImageDrawable(challenge.icon);
+        challengeTitle.setText(challenge.title);
+        challengeCurrentProgressTextView.setText("0/" + challenge.howManyToCollect);
+    }
+
+    public void endChallengeMode(boolean success) {
+        challengeMode = false;
+        currentChallenge = null;
+        challengesButton.setClickable(true);
+        challengesButton.setEnabled(true);
+        challengesButton.setAlpha(1f);
+        challengeTimerCountDown.cancel();
+        challengeLayout.setVisibility(GONE);
+        challengeCount = 0;
+        if (success) {
+            //TODO!!!
+            //showSuccessMessage();
+            //receiveReward();
+            Toast.makeText(this, "SUCCESS", Toast.LENGTH_LONG).show();
+        } else {
+            //showFailureMessage();
+            Toast.makeText(this, "FAIL", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
