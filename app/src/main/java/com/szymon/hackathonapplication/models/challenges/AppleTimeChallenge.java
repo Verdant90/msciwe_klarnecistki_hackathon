15a1b80514cfe24c638e58e9fa81148a39997bbb
Szymon <szymon.duraj.sp@lhsystems.com>
2018-04-14 14:39:53 +0200

    apple challenge icon

--- app/src/main/java/com/szymon/hackathonapplication/models/challenges/AppleTimeChallenge.java
+++ app/src/main/java/com/szymon/hackathonapplication/models/challenges/AppleTimeChallenge.java
@@ -3,7 +3,6 @@ package com.szymon.hackathonapplication.models.challenges;
 import com.szymon.hackathonapplication.R;
 import com.szymon.hackathonapplication.helpers.AppResources;
 
-import java.util.ArrayList;
 
 public class AppleTimeChallenge extends Challenge {
 
@@ -13,6 +12,6 @@ public class AppleTimeChallenge extends Challenge {
         description = "lorem ipsum";
         title = "lorem ipsum";
         howManyToCollect = 10;
-        icon = AppResources.getDrawable(R.drawable.ic_apple);
+        icon = AppResources.getDrawable(R.drawable.applechallenge);
     }
 }
