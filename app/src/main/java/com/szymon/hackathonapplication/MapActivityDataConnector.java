40eda8c7a2dc995fed9f06912bfea473f1a431cf
Szymon <szymon.duraj.sp@lhsystems.com>
2018-04-14 10:40:18 +0200

    MVP pattern added

---
+++ app/src/main/java/com/szymon/hackathonapplication/MapActivityDataConnector.java
@@ -0,0 +1,12 @@
+package com.szymon.hackathonapplication;
+
+import com.szymon.hackathonapplication.interfaces.MapMVP;
+
+public class MapActivityDataConnector implements MapMVP.Model {
+
+    private MapMVP.Presenter presenter;
+
+    public MapActivityDataConnector(final MapMVP.Presenter presenter) {
+        this.presenter = presenter;
+    }
+}
